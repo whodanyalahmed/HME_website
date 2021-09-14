7b59adbe8104e1438d6c24a24df45c42a3b8f449
whodanyalahmed <daniahmedkhatri@gmail.com>
2021-09-14 11:31:21 +0500

    login updated and classes add student added

--- resources/views/teachers/login.blade.php
+++ resources/views/teachers/login.blade.php
@@ -34,39 +34,48 @@
     </div>
     
 @endif
-<div class="registration-form mt-5 " id="login">
-    <form action="/teachers/dashboard" class="shadow rounded" method="POST">
-        @csrf
-        <h1 class="text-center mb-4 ">Login for Teachers</h1>
-        <div class="form-icon bg-success">
-            <span ><i class="far fa-user"></i></span>
+<div class="container">
+
+    <div class="row">
+        <div class="col-lg-6 text-center " style="margin-top: 100px;">
+            <h2 class="display-4 float-start">Login</h2>
+            <img class="d-lg-block d-xl-block d-none " src="/assets/img/teacher.svg" alt="teacher" height="500px" width="500px">
         </div>
-    <div class="form-group">
-        <input type="text" class="form-control item" id="email" name="email" placeholder="Email">
-    </div>
-    <div class="form-group">
-        <input type="password" class="form-control item" id="password" name="password"placeholder="Password">
-    </div>
-
-    <div class="form-group">
-        <div class="row">
-            <div class="col-md-6">
-
-                    <button type="submit" style=" border-radius: 30px !important;
-                    padding: 10px 20px !important;
-                    font-size: 18px;
-                    font-weight: bold;
-                    border: none;
-                    color: white;
-                    margin-top: 20px;"  class="btn btn-success btn-md rounded">Log in</button>
+        <div class="col-lg-6 col-md-12 d-flex justify-content-center justify-content-lg-start">
+        <div class="registration-form mt-5 float-start" id="login">
+            <form action="/teachers/dashboard" class="shadow rounded" method="POST">
+                @csrf
+                <h1 class="text-center mb-4 ">Login for Teachers</h1>
+                <div class="form-icon bg-success">
+                    <span ><i class="far fa-user"></i></span>
+                </div>
+            <div class="form-group">
+                <input type="text" class="form-control item" id="email" name="email" placeholder="Email">
             </div>
-            <div class="col-md-6">
-                <div class="mt-4"><a href="signup">Create an account?</a></div>
+            <div class="form-group">
+                <input type="password" class="form-control item" id="password" name="password"placeholder="Password">
             </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-6">
+
+                            <button type="submit" style=" border-radius: 30px !important;
+                            padding: 10px 20px !important;
+                            font-size: 18px;
+                            font-weight: bold;
+                            border: none;
+                            color: white;
+                            margin-top: 20px;"  class="btn btn-success btn-md rounded">Log in</button>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mt-4"><a href="signup">Create an account?</a></div>
+                    </div>
+                </div>
+            </div>
+        </form>
+
         </div>
     </div>
-</form>
-
 </div>
-
 @endsection
